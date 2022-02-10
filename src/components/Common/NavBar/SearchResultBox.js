cc2f8215346ed4b08b1eb1d446f6bbce8b97895a
ksw0154 <rlatkddnjs44@naver.com>
2022-02-11 06:04:22 +0900

    검색 결과 component 작성 및 캠핑장, 쏘카 기능 적용

---
+++ src/components/Common/NavBar/SearchResultBox.js
@@ -0,0 +1,95 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { connect } from "react-redux";
+import { updateStar } from "../../../store";
+
+const SearchResultBox = ({ searchResult, updateStar }) => {
+  const onClickStar = () => {
+    updateStar(searchResult);
+  };
+
+  return (
+    <ResultBox>
+      <Header>
+        <Title>{searchResult.placeName}</Title>
+        <Icons>
+          <Icon className="star" onClick={onClickStar}>
+            <FontAwesomeIcon style={{ color: "#f5a002" }} icon={faStar} />
+          </Icon>
+          <Icon>
+            <FontAwesomeIcon style={{ color: "#e01b35" }} icon={faHeart} />
+          </Icon>
+        </Icons>
+      </Header>
+      <div>
+        <Address>{searchResult.addressDoro}</Address>
+        <Address>(지번) {searchResult.addressJibun}</Address>
+      </div>
+      <Footer>
+        <Category>{searchResult.category}</Category>
+        <PlaceUrl href={searchResult.placeUrl} target="_blank" rel="noopener noreferrer">
+          상세보기
+        </PlaceUrl>
+      </Footer>
+    </ResultBox>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateStar: (result) => dispatch(updateStar(result)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SearchResultBox);
+
+const ResultBox = styled.div`
+  padding: 20px 20px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Icons = styled.div`
+  display: flex;
+`;
+
+const Icon = styled.div`
+  padding: 3px;
+  margin: 0 3px;
+  border: 1px solid lightgray;
+  font-size: 15px;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+  font-size: 17px;
+`;
+
+const Address = styled.p`
+  font-size: 12px;
+  line-height: 8px;
+  color: gray;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Category = styled.div`
+  font-size: 13px;
+`;
+
+const PlaceUrl = styled.a`
+  font-size: 12px;
+  margin-left: 8px;
+`;
---
+++ src/components/Common/NavBar/SearchResultList.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { getList } from "../../../store";
+import SearchResultBox from "./SearchResultBox";
+import styled from "styled-components";
+
+// import HOUSE from "../../../images/camp.png";
+// import GIFTSHOP from "../../../images/giftshop.png";
+// const { kakao } = window;
+
+const SearchResultList = ({ categoryInfo, getList }) => {
+  const [loading, setLoading] = useState(true);
+
+  const getResult = async (url) => {
+    const result = await axios.get(url);
+    getList(categoryInfo.icon.iconName, result.data.data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (categoryInfo && categoryInfo.list === "") {
+      setLoading(true);
+      getResult(categoryInfo.url);
+    } else {
+    }
+  }, [categoryInfo]);
+
+  // const kakaoMap = map;
+  // const [markerList, setMarkerList] = useState();
+  // const markers = [];
+
+  // const markersHandler = async (url, name) => {
+  //   try {
+  //     if (markers) {
+  //       for (let i = 0; i < markers.length; i++) {
+  //         markers[i].setMap(null);
+  //       }
+  //     }
+  //     const response = await axios.get(url);
+
+  //     response.data.name = name;
+  //     setMarkerList(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   if (markerList) {
+  //     let imageSrc;
+  //     let imageOption;
+  //     switch (markerList.name) {
+  //       case "JEJU_CAMPING_URL":
+  //         imageSrc = HOUSE;
+  //         // imageSrc = GIFTSHOP;
+  //         break;
+  //       case "JEJU_SOCAR_URL":
+  //         imageSrc = "https://www.pikpng.com/pngl/b/165-1656003_socar-png-clipart.png";
+
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //     let markerImage;
+  //     if (imageSrc) {
+  //       markerImage = new kakao.maps.MarkerImage(imageSrc, new kakao.maps.Size(24, 35), imageOption);
+  //     }
+  //     markerList.data.map((position) => {
+  //       let marker = new kakao.maps.Marker({
+  //         position: new kakao.maps.LatLng(position.latitude, position.longitude),
+  //         title: position.longitude,
+  //         image: markerImage ? markerImage : null,
+  //         clickable: true,
+  //       });
+
+  //       markers.push(marker);
+  //       return marker.setMap(kakaoMap);
+  //     });
+  //   }
+  // });
+  if (loading) {
+    return null;
+  }
+
+  if (!categoryInfo.list) {
+    return null;
+  }
+
+  return (
+    <ResultList>
+      {categoryInfo.list.map((searchResult, index) => (
+        <SearchResultBox key={index} searchResult={searchResult} />
+      ))}
+    </ResultList>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { categoryInfo: state.find((category) => category.focused === true) };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return { getList: (id, list) => dispatch(getList(id, list)) };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchResultList);
+
+const ResultList = styled.div`
+  margin-top: 10px;
+`;
