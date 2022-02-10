d8e5e2bc5a85485893d50ddc2568cee00f39d277
ksw0154 <rlatkddnjs44@naver.com>
2022-02-11 02:25:20 +0900

    BrowserRouter 위치 변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage/LandingPage";
 import KakaoAuth from "./components/Login/KakaoLogin/KakaoAuth";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/oauth/callback/kakao" element={<KakaoAuth />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/oauth/callback/kakao" element={<KakaoAuth />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
