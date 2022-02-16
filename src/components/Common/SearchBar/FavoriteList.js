1aa08404a3831f2939e14186609f7c61eb705572
ksw0154 <rlatkddnjs44@naver.com>
2022-02-16 19:18:59 +0900

    즐겨찾기 추가 UI 작성

--- src/components/Common/SearchBar/FavoriteList.js
+++ src/components/Common/SearchBar/FavoriteList.js
@@ -3,36 +3,46 @@ import styled from "styled-components";
 import { connect } from "react-redux";
 import FavoriteBox from "./FavoriteBox";
 import { getFavorites } from "../../../_reducers/user";
+import { useNavigate } from "react-router-dom";
 
-const FavoriteList = ({ favorites, getFavorites }) => {
+const FavoriteList = ({ userInfo, favorites, getFavorites }) => {
+  const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
+
+  const onClick = () => {
+    navigate("/login");
+  };
 
   useEffect(() => {
     getFavorites();
+    setIsLoading(false);
   }, []);
-
-  useEffect(() => {
-    if (favorites) {
-      setIsLoading(false);
-    }
-  }, [favorites]);
 
   if (isLoading) {
     return null;
   }
 
   return (
-    <ResultList>
-      {favorites.list.map((favorite, index) => (
-        <FavoriteBox key={index} searchResult={favorite} />
-      ))}
-    </ResultList>
+    <>
+      <MessageList>
+        {!userInfo && <LoginBtn onClick={onClick}>login 하러 가기</LoginBtn>}
+        {userInfo && (!favorites || favorites.length === 0) && <MessageBox>즐겨찾기 내용이 없습니다.</MessageBox>}
+      </MessageList>
+      {userInfo && favorites && (
+        <ResultList>
+          {favorites.list.map((favorite, index) => (
+            <FavoriteBox key={index} searchResult={favorite} />
+          ))}
+        </ResultList>
+      )}
+    </>
   );
 };
 
 const mapStateToProps = (state) => {
   return {
     favorites: state.user.favorites,
+    userInfo: state.user.kakao,
   };
 };
 
@@ -46,4 +56,31 @@ export default connect(mapStateToProps, mapDispatchToProps)(FavoriteList);
 
 const ResultList = styled.div`
   margin-top: 10px;
+  overflow-y: auto;
+  height: 100%;
+`;
+
+const MessageList = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const MessageBox = styled.div`
+  padding: 15px;
+  color: white;
+  border: none;
+  font-size: 13px;
+  border-radius: 15px;
+  background-color: #f5a002;
+`;
+
+const LoginBtn = styled.button`
+  padding: 15px;
+  color: white;
+  border: none;
+  border-radius: 15px;
+  background-color: #f5a002;
+  cursor: pointer;
 `;
